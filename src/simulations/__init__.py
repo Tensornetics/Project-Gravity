ad05c6ce1b255933b2866053f4964350c7c66a2e
Kyle Grant <95764976+K-MkrOps@users.noreply.github.com>
2023-02-21 22:35:12 -0800

    Create __init__.py
    This implementation defines the __init__.py file for the src/simulations package. The file imports functions from the other modules in the package and makes them available for import from the package.
    
    The file uses relative imports to import the compute_gravitational_field, compute_metric_tensor, compute_stress_energy_tensor, and run_simulation functions from the gravitational_field.py, metric_tensor.py, stress_energy_tensor.py, and run_simulation.py modules, respectively.
    
    By including the __init__.py file, the simulations package can be imported as a module, and the functions defined in the package can be used in other scripts or notebooks.

---
+++ src/simulations/__init__.py
@@ -0,0 +1,4 @@
+from .gravitational_field import compute_gravitational_field
+from .metric_tensor import compute_metric_tensor
+from .stress_energy_tensor import compute_stress_energy_tensor
+from .run_simulation import run_simulation
